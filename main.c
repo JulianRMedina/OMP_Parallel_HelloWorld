3c8e5ee296925dc17bb2cec63b1f195f573f6e75
JulianRMedina <JMedina@Julians-MacBook-Pro.local>
2016-10-24 21:00:18 -0600

    Added OMP Pragma's
    Added OMP Pragma Parallel code blocks

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
+#include <omp.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(){
-	printf("Hello World!");
+	int nthreads; int tid;
+		
+	//ask for threads giving them each a private copy of nthreads and tid
+#pragma omp parallel private(tid)
+	{
+		//get thread id and print Hello World from strings
+		tid = omp_get_thread_num();
+		printf("Hello World from thread %d!\n", tid);
+		
+		//only master counts and prints number of threads
+	#pragma omp master
+		{
+			nthreads = omp_get_num_threads();
+			printf("There is a total of %d threads.\n", nthreads);
+		}
+	}
+	
 	return 0;
 }
